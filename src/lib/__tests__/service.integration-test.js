f5b8b425cf8b812c9010bc4ab42ecf7f9fd0030a
Randy Singh <randy.singh@scotiabank.com>
2017-05-03 10:56:15 -0400

    RS - Add service integration tests.
    - Exposed bug with url concatenation and was fixed.

---
+++ src/lib/__tests__/service.integration-test.js
@@ -0,0 +1,65 @@
+import 'whatwg-fetch';
+import fetchMock from 'fetch-mock';
+
+import { buildService } from '../service';
+
+describe('service', () => {
+  const testEndpoint = 'http://myapp.com/users';
+  const cake = { name: 'Chocolate' };
+  const detailEndpoint = `${testEndpoint}/1`;
+
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  describe('#find', () => {
+    test('should make get request', () => {
+      fetchMock.get(testEndpoint, cake);
+      const service = buildService(testEndpoint);
+
+      return service.find().then((result) => {
+        expect(fetchMock.done(testEndpoint)).toEqual(true);
+        expect(result).toEqual(cake);
+      });
+    });
+  });
+
+  describe('#create', () => {
+    test('should make post request', () => {
+      fetchMock.post(testEndpoint, { response: 200 });
+
+      const service = buildService(testEndpoint);
+
+      return service.create(cake).then((result) => {
+        expect(fetchMock.done(testEndpoint)).toEqual(true);
+        expect(result).toEqual({ response: 200 });
+      });
+    });
+  });
+
+  describe('#update', () => {
+    test('should make put request', () => {
+      fetchMock.put(detailEndpoint, { response: 200 });
+
+      const service = buildService(testEndpoint);
+
+      return service.update('1', cake).then((result) => {
+        expect(fetchMock.done(detailEndpoint)).toEqual(true);
+        expect(result).toEqual({ response: 200 });
+      });
+    });
+  });
+
+  describe('#delete', () => {
+    test('should make delete request', () => {
+      fetchMock.delete(detailEndpoint, { response: 200 });
+
+      const service = buildService(testEndpoint);
+
+      return service.delete('1').then((result) => {
+        expect(fetchMock.done(detailEndpoint)).toEqual(true);
+        expect(result).toEqual({ response: 200 });
+      });
+    });
+  });
+});
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,13 +1,20 @@
+import url from 'url';
 import path from 'path';
 import api from './api';
 
 const json = (request) => request.then(result => result.json());
 
+const getUrl = (endpoint, ...paths) => {
+  let parsedUrl = url.parse(endpoint);
+  parsedUrl.pathname = path.join(parsedUrl.pathname, ...paths);
+  return url.format(parsedUrl);
+};
+
 export function buildService(endpoint) {
   return {
     find: () => json(api.get(endpoint)),
     create: (body) => json(api.post(endpoint, body)),
-    update: (id, body) => json(api.put(path.join(endpoint, id), body)),
-    delete: (id) => json(api.delete(path.join(endpoint, id)))
+    update: (id, body) => json(api.put(getUrl(endpoint, id), body)),
+    delete: (id) => json(api.delete(getUrl(endpoint, id)))
   };
 }
